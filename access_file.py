ff821f8941e7a52418b8ab06f8c0e7940fffffc2
raina-04 <155474255+raina-04@users.noreply.github.com>
2025-10-29 00:48:10 +0530

    Update access_file.py

--- access_file.py
+++ access_file.py
@@ -2,11 +2,12 @@ import json
 import pandas as pd
 
 # Load dataset
-with open("C:\\dbms\\FINAL_PROJ\\complaints_dataset.json", "r", encoding="utf-8") as f:
+with open("complaints_dataset (3).json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 df = pd.DataFrame(data)
 
 # Quick check
 print(df[["Section", "Offence", "Discription"]].head())
+
 print(f"Total IPC entries: {len(df)}")
